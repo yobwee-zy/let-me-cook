686648543fd3dbff0c9566b42422e9e117390f05
yobwee-zy <bkingeno07@gmail.com>
2024-01-20 02:07:14 +0300

    easy fix

--- LANDLORD_1.0/LANDLORD_2.0/landlord.py
+++ LANDLORD_1.0/LANDLORD_2.0/landlord.py
@@ -1,9 +1,9 @@
 from flask import Flask, render_template
-from flask_pymongo import PyMongo
+#from flask_pymongo import PyMongo
 
 app = Flask(__name__)
-app.config["MONGO_URI"] = 'mongodb://127.0.0.1:27017/landlord2.0'
-mongo = PyMongo(app)
+#app.config["MONGO_URI"] = 'mongodb://127.0.0.1:27017/landlord2.0'
+#mongo = PyMongo(app)
 
 @app.route('/index')
 def index():
